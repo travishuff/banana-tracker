ccdd90f612db95b7b7d5993c486c9c217370294c
travishuff <huff.travis@gmail.com>
2019-05-04 11:31:47 -0700

    Set up buy and sell API calls

--- src/Buy.js
+++ src/Buy.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import axios from 'axios'
 
 class Buy extends React.Component {
+  state = {
+    number: '',
+    buyDate: '',
+  }
+
+  handleNumberChange = event => {
+    if (!event.target.value) {
+      this.setState({
+        number: '',
+      })
+    } else {
+      this.setState({
+        number: Number(event.target.value),
+      })
+    }
+  }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+  handleSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/bananas',
+        this.state
+      )
+      console.log('submitted!', response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
-    return <div>Buy</div>
+    const { number, buyDate } = this.state
+
+    return (
+      <div>
+        <div>Buy</div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="number">
+            Number of bananas to buy
+            <input
+              type="number"
+              name="number"
+              value={number}
+              onChange={this.handleNumberChange}
+              onBlur={this.handleNumberChange}
+            />
+          </label>
+          <label htmlFor="buyDate">
+            Purchase date
+            <input
+              type="date"
+              name="buyDate"
+              value={buyDate}
+              onChange={this.handleChange}
+              onBlur={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
   }
 }
 
--- src/Sell.js
+++ src/Sell.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import axios from 'axios'
 
 class Sell extends React.Component {
+  state = {
+    number: '',
+    sellDate: '',
+  }
+
+  handleNumberChange = event => {
+    if (!event.target.value) {
+      this.setState({
+        number: '',
+      })
+    } else {
+      this.setState({
+        number: Number(event.target.value),
+      })
+    }
+  }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+  handleSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      const response = await axios.put(
+        'http://localhost:8080/api/bananas',
+        this.state
+      )
+      console.log('submitted!', response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
-    return <div>Sell</div>
+    const { number, sellDate } = this.state
+
+    return (
+      <div>
+        <div>Sell</div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="number">
+            Number of bananas to sell
+            <input
+              type="number"
+              name="number"
+              value={number}
+              onChange={this.handleNumberChange}
+              onBlur={this.handleNumberChange}
+            />
+          </label>
+          <label htmlFor="sellDate">
+            Sale date
+            <input
+              type="date"
+              name="sellDate"
+              value={sellDate}
+              onChange={this.handleChange}
+              onBlur={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
   }
 }
 
